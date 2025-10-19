a655bbbcd164cd582d88d93f6aa56344827621cb
Yunis-Konda001 <k.konda@alustudent.com>
2025-10-18 22:38:23 -0700

    Likelihood

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -1,65 +1,49 @@
 #!/usr/bin/env python3
-""" Implements the likelihood method
 """
+def likelihood(x, n, P): that calculates the likelihood
+of obtaining this data given various hypothetical
+probabilities of developing severe side effects
+"""
+
+
 import numpy as np
 
 
 def likelihood(x, n, P):
-    """ Calculates the likelihood of obtaining data given
-    different hypothetical probabilities
-    Args:
-        x (int) the number of patients who develop side-effects
-        n (int) the number of patients who have taken the drug
-        P (numpy.ndarray) the list of different hypothetical probabilites
-            of developing side-effects.
-    Returns:
-        numpy.ndarray the list of different likelihoods
     """
+    Args:
+        x is the number of patients that develop severe side effects
+        n is the total number of patients observed
+        P is a 1D numpy.ndarray of length equal to the
+        number of patients that develop severe side effects
+
+    Returns:
+        the likelihood of obtaining x and n
+    """
+    # Check if n is a positive integer
     if not isinstance(n, int) or n <= 0:
-        raise ValueError('n must be a positive integer')
+        raise ValueError("n must be a positive integer")
 
+    # Check if x is an integer and greater than or equal to 0
     if not isinstance(x, int) or x < 0:
-        msg = 'x must be an integer that is greater than or equal to 0'
-        raise ValueError(msg)
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
 
+    # check if x is greater than n
     if x > n:
-        raise ValueError('x cannot be greater than n')
+        raise ValueError("x cannot be greater than n")
 
-    if not isinstance(P, np.ndarray) or P.ndim != 1:
-        raise TypeError('P must be a 1D numpy.ndarray')
+    # Check if p is a 1D numpy.ndarray
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
 
-    if not np.all((P >= 0) & (P <= 1)):
-        raise ValueError('All values in P must be in the range [0, 1]')
+    # Check if all values in p are in the range [0, 1]
+    if np.any((P < 0) | (P > 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
 
-    return __choose(n, x) * P ** x * (1 - P) ** (n - x)
-
-
-def __choose(n, x):
-    """ Calculates the number of combinations of x items that can
-    be made by choosing values from a set of n items
-    Args:
-        n (int) the number of items to choose from
-        x (int) the number of items to choose at a time
-    Returns:
-        (int) the number of combinations
-    """
-    return __fact(n) / (__fact(x) * __fact(n - x))
-
-
-def __fact(n):
-    """ Calculates the factorial of a given number n
-    Args:
-        n (int) the number whose factorial is to be calculated
-    Returns:
-        (int) the factorial of n
-    """
-    if not isinstance(n, int):
-        raise TypeError('n is supposed to be an int')
-
-    if n < 0:
-        raise ValueError('n must be greater than or equal to zero')
-
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * __fact(n - 1)`
+    # Calculate the combination using np.math.factorial
+    factorial = np.math.factorial
+    fact_coefficient = factorial(n) / (factorial(n - x) * factorial(x))
+    likelihood = fact_coefficient * (P ** x) * ((1 - P) ** (n - x))
+    return likelihood
