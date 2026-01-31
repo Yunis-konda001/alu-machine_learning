10dce268682c8fd7cd29fd3de7911b868ab29e01
root <Yunis-Konda001>
2026-01-31 05:20:53 +0000

    normalization

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
+"""
+    function def normalization_constants(X): that calculates the normalization
+    (standardization) constants of a matrix:
+"""
+
 
 import numpy as np
-normalization_constants = __import__('0-norm_constants').normalization_constants
 
-if __name__ == '__main__':
-    np.random.seed(0)
-    a = np.random.normal(0, 2, size=(100, 1))
-    b = np.random.normal(2, 1, size=(100, 1))
-    c = np.random.normal(-3, 10, size=(100, 1))
-    X = np.concatenate((a, b, c), axis=1)
-    m, s = normalization_constants(X)
-    print(X)
-    print(m)
-    print(s)
+
+def normalization_constants(X):
+    '''
+    X is the numpy.ndarray of shape (m, nx) to normalize
+        m is the number of data points
+        nx is the number of features
+    '''
+    mean = np.mean(X, axis=0)
+    std = np.std(X, axis=0)
+    return mean, std
